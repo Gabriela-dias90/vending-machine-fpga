98f797e8c9f15777b94f02bb9500c3119a6cc825
Gabriela-dias90 <gabriela.albino@iesb.edu.br>
2026-05-03 13:55:27 -0300

    Delete Simulacoes directory

--- Simulacoes/vending_machine_datapath.vhd
+++
@@ -1,7 +0,0 @@
-Running: xsim.dir/tb_vending_machine_top_behav/xsimk -simmode gui -wdb tb_vending_machine_top_behav.wdb -simrunnum 0 -socket 42531
-Design successfully loaded
-Design Loading Memory Usage: 31648 KB (Peak: 32220 KB)
-Design Loading CPU Usage: 40 ms
-Simulation completed
-Simulation Memory Usage: 121584 KB (Peak: 178920 KB)
-Simulation CPU Usage: 70 ms
